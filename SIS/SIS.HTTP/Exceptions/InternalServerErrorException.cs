bd3c7f1fe84bc1294218805b236fb0d3bde46c57
Mariya Pirinova <Mariya Pirinova@MPIRINOVA>
2019-05-29 16:14:53 +0300

    Added exception methods, extension methods and enums

---
+++ SIS/SIS.HTTP/Exceptions/InternalServerErrorException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SIS.HTTP.Exceptions
+{
+    public class InternalServerErrorException : Exception
+    {
+        private const string InternalServerErrorExceptionDefaultMessage = "The Server has encountered an error.";
+
+        public InternalServerErrorException() : this(InternalServerErrorExceptionDefaultMessage)
+        {
+        }
+        public InternalServerErrorException(string name) : base(name)
+        {
+        }
+    }
+}
