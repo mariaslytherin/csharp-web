1920092dbb87fc546a407c0d2532a1bb2da7b135
Mariya Pirinova <Mariya Pirinova@MPIRINOVA>
2019-05-30 17:18:41 +0300

    Added headers namespace and requests namespace

--- SIS/SIS.HTTP/Headers/HttpHeader.cs
+++ SIS/SIS.HTTP/Headers/HttpHeader.cs
@@ -1,9 +1,14 @@
-﻿namespace SIS.HTTP.Headers
+﻿using SIS.HTTP.Common;
+
+namespace SIS.HTTP.Headers
 {
     public class HttpHeader
     {
         public HttpHeader(string key, string value)
         {
+            CoreValidator.ThrowIfNullOrEmpty(key, nameof(key));
+            CoreValidator.ThrowIfNullOrEmpty(value, nameof(value));
+
             this.Key = key;
             this.Value = value;
         }
