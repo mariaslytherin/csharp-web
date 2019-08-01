c72d9d93f9c584231bdad9f23e9e44705e2d77ba
mariaslytherin <nirvana4ever>
2019-08-01 14:49:01 +0300

    Finished data base settings, created some controllers and added views and some view models

--- SIS/src/Apps/SULS/SULS.Data/SULSContext.cs
+++ SIS/src/Apps/SULS/SULS.Data/SULSContext.cs
@@ -1,9 +1,16 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using SULS.Models;
 
 namespace SULS.Data
 {
     public class SULSContext : DbContext
     {
+        public DbSet<User> MyProperty { get; set; }
+
+        public DbSet<Problem> Problems { get; set; }
+
+        public DbSet<Submission> Submissions { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(DbSettings.ConnectionString);
@@ -12,6 +19,21 @@ namespace SULS.Data
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<User>()
+                .HasKey(user => user.Id);
+
+            modelBuilder.Entity<Problem>()
+                .HasKey(problem => problem.Id);
+
+            modelBuilder.Entity<Submission>()
+                 .HasKey(submission => submission.Id);
+
+            modelBuilder.Entity<Submission>()
+                .HasOne(submission => submission.User);
+
+            modelBuilder.Entity<Submission>()
+                .HasOne(submission => submission.Problem);
+
             base.OnModelCreating(modelBuilder); 
         }
     }
--- SIS/src/Apps/SULS/SULS.Models/User.cs
+++ SIS/src/Apps/SULS/SULS.Models/User.cs
@@ -11,6 +11,7 @@ namespace SULS.Models
         public string Username { get; set; }
 
         [Required]
+        [EmailAddress]
         public string Email { get; set; }
 
         [Required]
